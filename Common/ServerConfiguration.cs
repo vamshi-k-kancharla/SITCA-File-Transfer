2a20749bfa1a8238aa51040a2637d943eda734a9
Vamshi Krishna Kancharla <vamshi.k.kancharla@gmail.com>
2023-06-29 19:52:00 +0530

    SITCA File Transfer : Last Chunk Fix + Number of file parts query enable

--- Common/ServerConfiguration.cs
+++ Common/ServerConfiguration.cs
@@ -4,7 +4,7 @@ namespace SITCAFileTransferService.Common
     public class FileTransferServerConfig
     {
         public static string inputFilePath = @"I:\SITCA File Transfer\SITCA Web Service\SourceDirectory\";
-        public static int chunkSize = 1000000;
+        public static int chunkSize = 30;
 
         public static bool bDebug = false;
     }
--- Controllers/FileTransferController.cs
+++ Controllers/FileTransferController.cs
@@ -5,7 +5,6 @@ using System.Diagnostics;
 using System.IO;
 
 using SITCAFileTransferService.Common;
-using MongoDB.Bson.Serialization.IdGenerators;
 
 namespace SITCAFileTransferService.Controllers
 {
@@ -47,6 +46,7 @@ namespace SITCAFileTransferService.Controllers
                 
                 int totalNumberOfBytesRead = 0;
                 int currentIterationCount = 0;
+                int totalNumberOfFilePartsLoaded = 0;
 
 
                 byte[] bytesToBeRead = new byte[FileTransferServerConfig.chunkSize];
@@ -75,11 +75,31 @@ namespace SITCAFileTransferService.Controllers
                     int currentSizeFileRead = (fileReadRetValue < FileTransferServerConfig.chunkSize) ?
                         fileReadRetValue : FileTransferServerConfig.chunkSize;
 
+                    Console.WriteLine("Current size of filePart Read = " + currentSizeFileRead);
+
+                    // Handle Last Read separately
+
+                    byte[] bytesToBeReadLastChunk = new byte[currentSizeFileRead];
+
+                    if ( currentSizeFileRead < FileTransferServerConfig.chunkSize )
+                    {
+                        
+                        for( int i = 0; i < currentSizeFileRead; i++)
+                        {
+                            bytesToBeReadLastChunk[i] = bytesToBeRead[i];
+                        }
+
+                    }
+
                     // Add bytes data to mongo DB.
 
                     FilePartsData newFilePartsToBeAdded = AddDataToCollection(currentIterationCount+1,
-                        "File-Part-" + currentIterationCount, bytesToBeRead);
+                        "File-Part-" + currentIterationCount,
+                        (currentSizeFileRead < FileTransferServerConfig.chunkSize) ? bytesToBeReadLastChunk
+                        : bytesToBeRead);
+
                     currentCollection.InsertOne(newFilePartsToBeAdded);
+                    totalNumberOfFilePartsLoaded++;
 
                     // Read next byte set of data
 
@@ -87,7 +107,7 @@ namespace SITCAFileTransferService.Controllers
 
                     if (FileTransferServerConfig.bDebug == true)
                     {
-                        retValueString += " Bytes read : Sub Sequent Read , currentOffset = " + currentOffset +
+                        retValueString += " Bytes read : SubSequent Read , currentOffset = " + currentOffset +
                         "chunkSize = " + FileTransferServerConfig.chunkSize + " , fileReadRetValue = " + fileReadRetValue;
                     }
 
@@ -108,7 +128,19 @@ namespace SITCAFileTransferService.Controllers
 
                 }
 
-                Console.WriteLine(" ,Total Number of Bytes Read = " + totalNumberOfBytesRead);
+                Console.WriteLine(" ,Total Number of Bytes Read = " + totalNumberOfBytesRead + 
+                    " , Total Number of file parts read " + totalNumberOfFilePartsLoaded);
+
+                
+                // Add Total number of Parts Data
+
+                byte[] noOfPartsByteArray = ConvertIntToByteArray(totalNumberOfFilePartsLoaded);
+
+                FilePartsData numberOfFilePartsAddedData = AddDataToCollection(currentIterationCount + 1,
+                    "NumberOfFileParts", noOfPartsByteArray);
+
+                currentCollection.InsertOne(numberOfFilePartsAddedData);
+
 
                 Console.WriteLine(" , End of read stream , current time = " + DateTime.Now.Hour + ":" +
                     DateTime.Now.Minute + ":" + DateTime.Now.Second + ":" + DateTime.Now.Millisecond);
@@ -120,10 +152,79 @@ namespace SITCAFileTransferService.Controllers
             {
 
                 logger.LogInformation("Exception occured while Loading the file into FilePartsData : Exception = " + e.Message);
+                return Results.BadRequest("Exception occured while loading the input file  : exception = " + e.Message);
             }
 
             return Results.Ok(retValueString);
         }
+
+        [HttpPost]
+        [Route("GetFilePartData/{fileName?}/{filePartName?}")]
+        public IResult GetFilePartData(string fileName, string filePartName)
+        {
+
+            byte[] retValueFilePartsData = new byte[FileTransferServerConfig.chunkSize];
+            string retValueString = "";
+
+            try
+            {
+                
+                IMongoCollection<FilePartsData> currentCollection = currentDB.GetCollection<FilePartsData>(fileName);
+
+
+                Console.WriteLine("=========================================================================");
+                Console.WriteLine(" , Before query execution  , current time = " + DateTime.Now.Hour + ":" +
+                    DateTime.Now.Minute + ":" + DateTime.Now.Second + ":" + DateTime.Now.Millisecond);
+
+                IFindFluent<FilePartsData, FilePartsData> queriedFilePartData  = 
+                    currentCollection.Find(x => x.filePartName == filePartName);
+
+                if (queriedFilePartData == null)
+                {
+                    Console.Write("Null data found for input query");
+                    throw new ArgumentNullException("File data for the input query doesn't exist");
+                }
+
+                var filePartsDataResponse = queriedFilePartData.FirstOrDefault();
+
+                if (filePartsDataResponse == null)
+                {
+                    Console.Write("Null data found for input query. After firstOrDefault ");
+                    throw new ArgumentNullException("File data for the input query doesn't exist");
+                }
+
+                retValueFilePartsData = filePartsDataResponse.filePartData;
+
+                if (retValueFilePartsData == null)
+                {
+                    Console.Write("Null data found for input query, after filePartData");
+                    throw new ArgumentNullException("File data for the input query doesn't exist");
+                }
+
+                Console.WriteLine(" , After query execution , before processing = " + DateTime.Now.Hour + ":" +
+                    DateTime.Now.Minute + ":" + DateTime.Now.Second + ":" + DateTime.Now.Millisecond);
+
+                for ( int i = 0; i < retValueFilePartsData.Length; i++)
+                {
+                    retValueString += (char)retValueFilePartsData[i];
+                }
+
+                Console.WriteLine(" , After query execution , current time = " + DateTime.Now.Hour + ":" +
+                    DateTime.Now.Minute + ":" + DateTime.Now.Second + ":" + DateTime.Now.Millisecond);
+
+                Console.WriteLine("=========================================================================");
+
+            }
+
+            catch (Exception e)
+            {
+                logger.LogInformation("Exception occured while querying the file parts Data : Exception = " + e.Message);
+                return Results.BadRequest("Exception occured while querying the file parts Data  : exception = " + e.Message);
+            }
+
+            return Results.Ok(retValueString);
+        }
+
 
 
         // Create DB Collection
@@ -147,6 +248,21 @@ namespace SITCAFileTransferService.Controllers
 
             return currentFileData;
 
+        }
+
+        byte[] ConvertIntToByteArray(int inputNum)
+        {
+
+            string strTotalFilePartsLoaded = Convert.ToString(inputNum);
+
+            byte[] noOfPartsByteArray = new byte[strTotalFilePartsLoaded.Length];
+
+            for ( int i = 0; i < strTotalFilePartsLoaded.Length; i++ )
+            {
+                noOfPartsByteArray[i] = (byte)strTotalFilePartsLoaded[i];
+            }
+
+            return noOfPartsByteArray;
         }
 
     }
